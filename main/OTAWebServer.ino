f64b260896d5e35b98a761dcb18413c33e82b93b
shogunxam <mario.damico75@gmail.com>
2019-11-09 17:32:55 +0100

    Updated the release version

--- main/OTAWebServer.ino
+++ main/OTAWebServer.ino
@@ -151,7 +151,7 @@ void OTAWebServer::setup(const String& hN, const String& _ssid_, const String& _
     serverInfo+="<h1>"GATEWAY_NAME"<h1>";
     serverInfo+="<h2>System Information</h2>";
     serverInfo+="<table style='width:100%'>";
-    serverInfo+="<tr><th>Program Version<th><td>"VERSION"</td></tr>";
+    serverInfo+="<tr><th>Firmware Version<th><td>"VERSION"</td></tr>";
     std::string uptime = formatMillis(millis());
     serverInfo+="<tr><th>Uptime<th><td>"; serverInfo+=uptime.c_str(); serverInfo+=+"</td></tr>";
     serverInfo+="<tr><th>SSID<th><td>"WIFI_SSID"</td></tr>";
@@ -160,6 +160,7 @@ void OTAWebServer::setup(const String& hN, const String& _ssid_, const String& _
     serverInfo+="<table style='width:100%'>";
     serverInfo+="<tr><th>Device</th><th>RSSI</th><th>Battery</th><th>State</th></tr>";
 
+    //Perhaps we need a mutex here to protect BLETrackedDevices but we are in readonly mode
     for (int i=0;i<NB_OF_BLE_DISCOVERED_DEVICES; i++)
     {
       std::ostringstream row;
