a43179e75137bafdced750efcd148ffdb380023e
shounxam <mario.damico75@gmail.com>
2021-01-19 21:53:33 +0100

    Set default value to 30 sec

--- main/main.ino
+++ main/main.ino
@@ -12,7 +12,7 @@
 // MQTT_KEEPALIVE : keepAlive interval in Seconds
 // Keepalive timeout for default MQTT Broker is 10s
 #ifndef MQTT_KEEPALIVE
-#define MQTT_KEEPALIVE 10
+#define MQTT_KEEPALIVE 30
 #endif
 
 // MQTT_SOCKET_TIMEOUT: socket timeout interval in Seconds
