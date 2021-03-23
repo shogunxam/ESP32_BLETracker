shogunxam/ESP32_BLETracker

commit 0db9c39faeb9f5936feb9ab186ae42bba62e0f1e

Add support for device friendly name (#41)
* Add friendly name to devices
* Display device's friendly name in the sysinfo page

main/settings.cpp
@@ -2,9 +2,35 @@
 #include "config.h"
 #include "DebugPrint.h"
 
-#define CURRENT_SETTING_VERSION 4
+#define CURRENT_SETTING_VERSION 5
 
 Settings SettingsMngr;
+
+Settings::KnownDevice::KnownDevice(const KnownDevice &dev)
+{
+    *this = dev;
+}
+
+Settings::KnownDevice::KnownDevice(const char *mac, bool batt, const char *desc)
+{
+    strncpy(address, mac, ADDRESS_STRING_SIZE);
+    strncpy(description, desc, DESCRIPTION_STRING_SIZE);
+    readBattery = batt;
+}
+
+Settings::KnownDevice::KnownDevice()
+{
+    address[0] = '\0';
+    description[0] = '\0';
+    readBattery = false;
+}
+
+Settings::KnownDevice &Settings::KnownDevice::operator=(const KnownDevice &dev)
+{
+    memcpy(address, dev.address, ADDRESS_STRING_SIZE);
+    memcpy(description, dev.description, DESCRIPTION_STRING_SIZE);
+    readBattery = dev.readBattery;
+}
 
 String Settings::ArrayToStringList(const std::vector<String> &whiteList)
 {
@@ -57,20 +83,14 @@ Settings::Settings(const String &fileName, bool emptyLists) : settingsFile(fileN
 
 void Settings::FactoryReset(bool emptyLists)
 {
+    KnownDevice d = {"CA67347FD139", true, "Nut Mario"};
     if (emptyLists)
     {
-        trackWhiteList.clear();
-        batteryWhiteList.clear();
+        knownDevices.clear();
     }
     else
     {
-#ifdef BLE_TRACKER_WHITELIST
-        trackWhiteList = {BLE_TRACKER_WHITELIST};
-#endif
-
-#ifdef BLE_BATTERY_WHITELIST
-        batteryWhiteList = {BLE_BATTERY_WHITELIST};
-#endif
+        knownDevices = {BLE_KNOWN_DEVICES_LIST};
     }
 
     enableWhiteList = ENABLE_BLE_TRACKER_WHITELIST;
@@ -88,19 +108,38 @@ void Settings::FactoryReset(bool emptyLists)
 std::size_t Settings::GetMaxNumOfTraceableDevices()
 {
     const std::size_t absoluteMaxNumOfTraceableDevices = 90;
-    const std::size_t minNumOfTraceableDevices = std::min(trackWhiteList.size() + 1,absoluteMaxNumOfTraceableDevices);
+    const std::size_t minNumOfTraceableDevices = std::min(knownDevices.size() + 1, absoluteMaxNumOfTraceableDevices);
     return enableWhiteList ? minNumOfTraceableDevices : absoluteMaxNumOfTraceableDevices;
 }
 
-void Settings::AddDeviceToWhiteList(const String &mac, bool checkBattery)
+Settings::KnownDevice *Settings::GetDevice(const String &value)
 {
-    if (!InWhiteList(mac, trackWhiteList))
-        trackWhiteList.push_back(mac);
-
-    if (checkBattery)
+    for (uint8_t j = 0; j < knownDevices.size(); j++)
     {
-        if (!InWhiteList(mac, batteryWhiteList))
-            batteryWhiteList.push_back(mac);
+        if (value == knownDevices[j].address)
+        {
+            return &(knownDevices[j]);
+        }
+    }
+
+    return nullptr;
+}
+
+void Settings::AddDeviceToList(const Settings::KnownDevice &device)
+{
+    if (!IsPropertyForDeviceEnabled(device.address, DeviceProperty::eTraceable))
+        knownDevices.push_back(device);
+}
+
+void Settings::AddDeviceToList(const char mac[ADDRESS_STRING_SIZE], bool checkBattery, const char description[DESCRIPTION_STRING_SIZE])
+{
+    if (!IsPropertyForDeviceEnabled(mac, DeviceProperty::eTraceable))
+    {
+        KnownDevice device;
+        memcpy(&device.address, mac, sizeof(device.address));
+        memcpy(&device.description, description, sizeof(device.description));
+        device.readBattery = checkBattery;
+        knownDevices.push_back(std::move(device));
     }
 }
 
@@ -122,19 +161,22 @@ String Settings::toJSON()
     data += R"("mqtt_pwd":")" + mqttPwd + R"(",)";
     data += R"("scanPeriod":)" + String(scanPeriod) + ",";
     data += R"("maxNotAdvPeriod":)" + String(maxNotAdvPeriod) + ",";
-    data += R"("loglevel":)"+ String(logLevel) + ",";
+    data += R"("loglevel":)" + String(logLevel) + ",";
     data += R"("whiteList":)";
     data += (enableWhiteList ? "true" : "false");
     data += R"(,)";
     data += R"("trk_list":{)";
     bool first = true;
-    for (auto &mac : trackWhiteList)
+    for (auto &device : knownDevices)
     {
         if (!first)
             data += ",";
         else
             first = false;
-        data += R"(")" + mac + R"(":)" + (InBatteryList(mac) ? "true" : "false");
+        data += R"(")" + String(device.address) + R"(":{)";
+        data += R"("battery":)";
+        data += (device.readBattery ? "true" : "false");
+        data += R"(,"desc":")" + String(device.description) + R"("})";
     }
     data += "}";
     data += "}";
@@ -144,7 +186,7 @@ String Settings::toJSON()
 bool Settings::IsTraceable(const String &value)
 {
     if (enableWhiteList)
-        return InWhiteList(value, trackWhiteList);
+        return IsPropertyForDeviceEnabled(value, DeviceProperty::eReadBattery);
     else
         return true;
 }
@@ -152,22 +194,31 @@ bool Settings::IsTraceable(const String &value)
 bool Settings::InBatteryList(const String &value)
 {
 #if PUBLISH_BATTERY_LEVEL
-    return InWhiteList(value, batteryWhiteList);
+    return IsPropertyForDeviceEnabled(value, DeviceProperty::eTraceable);
 #else
     return false;
 #endif
 }
 
-bool Settings::InWhiteList(const String &value, const std::vector<String> &whiteList)
+bool Settings::IsPropertyForDeviceEnabled(const String &value, DeviceProperty property)
 {
-    bool inWhiteList = false;
-    for (uint8_t j = 0; j < whiteList.size(); j++)
-        if (value == whiteList[j])
+    KnownDevice *device = GetDevice(value);
+
+    if (device != nullptr)
+    {
+        switch (property)
         {
-            inWhiteList = true;
+        case DeviceProperty::eTraceable:
+            return true;
             break;
+        case DeviceProperty::eReadBattery:
+            return device->readBattery;
+        default:
+            return false;
         }
-    return inWhiteList;
+    }
+
+    return false;
 }
 
 void Settings::SaveString(File file, const String &str)
@@ -177,13 +228,54 @@ void Settings::SaveString(File file, const String &str)
     file.write((uint8_t *)str.c_str(), strLen + 1);
 }
 
-void Settings::SaveStringArray(File file, const std::vector<String> &vstr)
+void Settings::SaveKnownDevices(File file)
 {
     size_t vstrLen;
-    vstrLen = vstr.size();
+    vstrLen = knownDevices.size();
     file.write((uint8_t *)&vstrLen, sizeof(vstrLen));
-    for (auto &str : vstr)
-        SaveString(file, str);
+    for (auto &device : knownDevices)
+    {
+        file.write((uint8_t *)&device, sizeof(KnownDevice));
+    }
+}
+
+void Settings::LoadKnownDevices(File file, uint16_t version)
+{
+    knownDevices.clear();
+    if (version > 4)
+    {
+        size_t vstrLen;
+        file.read((uint8_t *)&vstrLen, sizeof(vstrLen));
+        for (size_t i = 0; i < vstrLen; i++)
+        {
+            KnownDevice device;
+            file.read((uint8_t *)&device, sizeof(KnownDevice));
+            knownDevices.push_back(device);
+        }
+    }
+    else //Build KnowDevices from 2 arrays
+    {
+        std::vector<String> batteryWhiteList;
+        std::vector<String> trackWhiteList;
+        LoadStringArray(file, batteryWhiteList);
+        LoadStringArray(file, trackWhiteList);
+        for (const auto &mac : trackWhiteList)
+        {
+            KnownDevice dev;
+            dev.readBattery = false;
+            dev.description[0] = '\0';
+            strncpy(dev.address, mac.c_str(), ADDRESS_STRING_SIZE);
+            for (const auto &macBatt : batteryWhiteList)
+            {
+                if (mac == macBatt)
+                {
+                    dev.readBattery = true;
+                    break;
+                }
+            }
+            knownDevices.push_back(dev);
+        }
+    }
 }
 
 void Settings::LoadString(File file, String &str)
@@ -213,8 +305,7 @@ bool Settings::Save()
         SaveString(file, mqttUser);
         SaveString(file, mqttPwd);
         file.write((uint8_t *)&enableWhiteList, sizeof(enableWhiteList));
-        SaveStringArray(file, batteryWhiteList);
-        SaveStringArray(file, trackWhiteList);
+        SaveKnownDevices(file);
         file.write((uint8_t *)&scanPeriod, sizeof(scanPeriod));
         file.write((uint8_t *)&logLevel, sizeof(logLevel));
         file.write((uint8_t *)&maxNotAdvPeriod, sizeof(maxNotAdvPeriod));
@@ -238,8 +329,7 @@ void Settings::Load()
         LoadString(file, mqttUser);
         LoadString(file, mqttPwd);
         file.read((uint8_t *)&enableWhiteList, sizeof(enableWhiteList));
-        LoadStringArray(file, batteryWhiteList);
-        LoadStringArray(file, trackWhiteList);
+        LoadKnownDevices(file, currVer);
         if (currVer > 1)
             file.read((uint8_t *)&scanPeriod, sizeof(scanPeriod));
         if (currVer > 2)
@@ -249,7 +339,7 @@ void Settings::Load()
     }
 }
 
-const std::vector<String>& Settings::GetTrackWhiteList()
+const std::vector<Settings::KnownDevice> &Settings::GetKnownDevicesList()
 {
-    return trackWhiteList;
+    return knownDevices;
 }

main/OTAWebServer.cpp
@@ -15,7 +15,6 @@
 #include "OTAWebServer.h"
 #include "macro_utility.h"
 #include "settings.h"
-
 #include "SPIFFSLogger.h"
 
 extern std::vector<BLETrackedDevice> BLETrackedDevices;
@@ -390,10 +389,39 @@ void OTAWebServer::postUpdateConfig()
       newSettings.maxNotAdvPeriod = server.arg(i).toInt();
     else if (server.argName(i) == "whiteList")
       newSettings.EnableWhiteList(server.arg(i) == "true");
-    else //other are mac address with battery check in the form "AE13FCB45BAD":"true"
+    else //other are mac address with properties in the form "MACADDRESS[property]":"value"
     {
-      bool batteryCheck = server.arg(i) == "true";
-      newSettings.AddDeviceToWhiteList(server.argName(i), batteryCheck);
+      char argName[20]; //MacAddress size (12 chars) + 2 square brackets + property name size (5 chars) + terminator
+      strncpy(argName, server.argName(i).c_str(), 20);
+      char seps[] = "[]";
+      char *token = strtok(argName, seps);
+      if (token != nullptr)
+      {
+        //First token is the Mac Address
+        DEBUG_PRINTF("Device: '%s'\n", token);
+        Settings::KnownDevice *device = newSettings.GetDevice(token);
+        if (nullptr == device)
+        {
+          Settings::KnownDevice tDev;
+          strncpy(tDev.address, token, ADDRESS_STRING_SIZE);
+          newSettings.AddDeviceToList(tDev);
+          device = newSettings.GetDevice(token);
+        }
+
+        if (nullptr != device)
+        {
+          token = strtok(nullptr, seps);
+          if (token != nullptr)
+          {
+            //Second token is the property name
+            DEBUG_PRINTF("Match: '%s' has value '%s' \n", token, server.arg(i).c_str());
+            if (strcmp(token, "batt") == 0)
+              device->readBattery = server.arg(i) == "true";
+            else if (strcmp(token, "desc") == 0)
+              strncpy(device->description, server.arg(i).c_str(), DESCRIPTION_STRING_SIZE);
+          }
+        }
+      }
     }
   }
 
@@ -446,7 +474,11 @@ void OTAWebServer::getSysInfoData()
     else
       SendChunkedContent(",");
     SendChunkedContent(R"({"mac":")");
-    SendChunkedContent(trackedDevice.address);
+    Settings::KnownDevice *device = SettingsMngr.GetDevice(trackedDevice.address);
+    if (device != nullptr && device->description[0] != '\0')
+      SendChunkedContent(device->description);
+    else
+      SendChunkedContent(trackedDevice.address);
     SendChunkedContent(R"(",)");
     SendChunkedContent(R"("rssi":)");
     itoa(trackedDevice.rssiValue, strbuff, 10);
@@ -590,26 +622,26 @@ void OTAWebServer::setup(const String &hN, const String &_ssid_, const String &_
 }
 
 static std::atomic_ulong WebServerWatchDogStartTime(0);
-void WebServerWatchDogloop(void * param)
+void WebServerWatchDogloop(void *param)
 {
-    WebServer *server = (WebServer *)param;
-    WebServerWatchDogStartTime.store(millis());
-    while (true)
+  WebServer *server = (WebServer *)param;
+  WebServerWatchDogStartTime.store(millis());
+  while (true)
+  {
+    if (millis() > (WebServerWatchDogStartTime.load() + 10000))
     {
-        if (millis() > (WebServerWatchDogStartTime.load() + 10000))
-        {
-            server->stop();
-            server->begin();
-            DEBUG_PRINTLN("INFO: WebServerWatchdog resart server");
-            LOG_TO_FILE_E("Error: WebServerWatchdog resart server");
-        }
-        delay(1000);
-    };
+      server->stop();
+      server->begin();
+      DEBUG_PRINTLN("INFO: WebServerWatchdog resart server");
+      LOG_TO_FILE_E("Error: WebServerWatchdog resart server");
+    }
+    delay(1000);
+  };
 }
 
 void WebServerWatchDogFeed()
 {
-    WebServerWatchDogStartTime.store(millis());
+  WebServerWatchDogStartTime.store(millis());
 }
 
 void WebServerLoop(void *param)
@@ -625,7 +657,7 @@ void WebServerLoop(void *param)
     {
       server->handleClient();
       WebServerWatchDogFeed();
-      if(restart)
+      if (restart)
       {
         restart = false;
         server->stop();
@@ -662,10 +694,10 @@ void OTAWebServer::begin(void)
   xTaskCreatePinnedToCore(
       WebServerWatchDogloop,   /* Function to implement the task */
       "WebServerWatchDogloop", /* Name of the task */
-      3072,           /* Stack size in words */
-      (void *)&server, /* Task input parameter */
-      10,             /* Priority of the task */
-      NULL,           /* Task handle. */
+      3072,                    /* Stack size in words */
+      (void *)&server,         /* Task input parameter */
+      10,                      /* Priority of the task */
+      NULL,                    /* Task handle. */
       1);
 }
 #endif /*ENABLE_OTA_WEBSERVER*/

main/html/config.html
@@ -7,7 +7,7 @@
 </head>
 
 <body>
-	<form id="form" name="config" action="">
+	<form id="configform" name="config" action="">
 		<div id="mqttbroker">
 			<fieldset>
 				<legend>MQTT Broker:</legend>

main/html/config.html.gz.h
@@ -1,65 +1,65 @@
 //Don't edit this file, it's automatically generated
-const long int config_html_gz_size = 975;
+const long int config_html_gz_size = 970;
 const unsigned char config_html_gz[] = {
-0x1F,0x8B,0x08,0x08,0xCE,0xE1,0x55,0x60,0x02,0xFF,0x63,0x6F,0x6E,0x66,0x69
+0x1F,0x8B,0x08,0x08,0x6C,0x99,0x57,0x60,0x02,0xFF,0x63,0x6F,0x6E,0x66,0x69
 ,0x67,0x2E,0x68,0x74,0x6D,0x6C,0x00,0x9D,0x56,0x6D,0x6F,0xDB,0x36,0x10,0xFE,0x1C
 ,0xFF,0x0A,0x82,0x40,0x91,0x18,0x68,0xAC,0x66,0x6B,0x31,0x24,0x91,0x04,0x24,0x29
-,0x02,0x0C,0x68,0xD6,0x0E,0x49,0xF7,0x75,0xA0,0xC4,0xB3,0xC4,0x98,0x22,0x55,0x92
-,0xF2,0x0B,0x86,0xFD,0xF7,0x1E,0x45,0x49,0x96,0xEC,0x76,0x4B,0x6B,0xC0,0x36,0x79
+,0x02,0x0C,0x58,0xBA,0x0E,0x49,0xF7,0x75,0xA0,0xC4,0xB3,0xC4,0x98,0x22,0x55,0x92
+,0xF2,0x0B,0x86,0xFD,0xF7,0x1E,0x45,0x49,0x96,0xEC,0x76,0x48,0x6A,0xC0,0x36,0x79
 ,0xE4,0x3D,0xF7,0xDC,0xF1,0xEE,0xC8,0xB8,0x74,0x95,0x4C,0x67,0xB3,0xB8,0x04,0xC6
 ,0xD3,0xD9,0x49,0x6C,0x73,0x23,0x6A,0x47,0xAC,0xC9,0x13,0x5A,0x3A,0x57,0xDB,0xAB
 ,0x28,0x62,0xCF,0x6C,0xBB,0x28,0xB4,0x2E,0x24,0xB0,0x5A,0xD8,0x45,0xAE,0xAB,0x56
-,0x16,0x49,0x91,0xD9,0xE8,0xF9,0x4B,0x03,0x66,0x17,0xFD,0xBA,0x78,0xB7,0xB8,0xE8
+,0x16,0x49,0x91,0xD9,0xE8,0xF9,0x6B,0x03,0x66,0x17,0xFD,0xBA,0xF8,0xB0,0xB8,0xE8
 ,0x26,0x8B,0x4A,0xA8,0xC5,0xB3,0xA5,0x69,0x1C,0x05,0xBC,0x43,0xE4,0x5C,0xAB,0xA5
 ,0x28,0x8E,0xB6,0x48,0xA1,0x56,0xC4,0x80,0x4C,0xA8,0x75,0x3B,0x09,0xB6,0x04,0x70
 ,0x94,0xB8,0x5D,0x0D,0x09,0x75,0xB0,0x75,0x51,0x6E,0x2D,0x25,0xA5,0x81,0x65,0xB7
 ,0x63,0xE1,0x05,0xE9,0x2C,0x8E,0x02,0xFD,0x59,0x9C,0x69,0xBE,0xF3,0x48,0x4B,0x6D
-,0x2A,0x22,0x78,0x42,0xFD,0x80,0x12,0xC5,0x2A,0xE8,0xAD,0x52,0xC2,0x72,0x27,0xB4
-,0x4A,0x28,0x6A,0x9E,0x9C,0xC4,0x5C,0xAC,0xDB,0x9D,0xD5,0x17,0xE7,0x32,0xA3,0x57
-,0x60,0x5A,0x39,0x62,0x08,0x90,0xDC,0x82,0x6B,0x67,0x48,0x0E,0x0A,0x50,0x3C,0x7D
-,0xF8,0xF3,0xE9,0x89,0xDC,0xB6,0xFB,0xAE,0xE2,0xA8,0x13,0x86,0x1D,0x8E,0x65,0x12
-,0xC2,0x18,0x27,0xA6,0x1B,0xE1,0x90,0xA7,0xB1,0x64,0x19,0x48,0x82,0x6C,0x82,0x21
-,0x6B,0xD6,0x68,0xE6,0x86,0x73,0x03,0xD6,0x7A,0x1C,0xBF,0x8C,0xB1,0x70,0x7C,0xA2
-,0x25,0x54,0xDD,0xB8,0x51,0x00,0xE8,0xC0,0xB4,0x05,0xE8,0xFC,0xDA,0x03,0x8E,0x00
-,0x70,0x68,0x5E,0xC6,0xA5,0xD6,0xC6,0xD1,0xF4,0x13,0xFE,0xBE,0x8C,0x88,0x6A,0xAA
-,0x0C,0x83,0x34,0x50,0x69,0xF5,0x47,0x54,0x02,0xDE,0x4F,0x51,0x69,0x2C,0x3A,0xF1
-,0xD9,0x86,0xD0,0xFE,0x68,0x48,0xBC,0xF2,0x88,0x46,0x8B,0xF5,0x73,0x01,0xD9,0x70
-,0x8C,0x07,0xB3,0x76,0xA3,0x0D,0x7F,0x19,0x93,0xBA,0xDB,0x3D,0x8A,0x0A,0x82,0x8C
-,0x83,0xE2,0x31,0xBF,0xC9,0x06,0x47,0x43,0xDE,0xC4,0xD1,0x38,0xE9,0xE2,0x08,0x73
-,0xB3,0x1D,0x4C,0x52,0xB1,0xCF,0xC4,0xF7,0xB0,0x16,0x39,0x58,0xE2,0x34,0x71,0x86
-,0xE5,0xAB,0x49,0x36,0x8E,0x7D,0xB2,0x39,0x53,0x35,0x18,0xA1,0x91,0x51,0x5B,0x36
-,0x09,0xDD,0x08,0xEE,0xCA,0x2B,0xD6,0xA0,0x6E,0x09,0xA2,0x28,0x5D,0x3B,0xA6,0xE9
-,0x23,0x6E,0x25,0x61,0xEF,0xE0,0x78,0x0B,0xF7,0xBD,0x04,0x18,0x63,0x07,0x6F,0xC7
-,0x12,0xEC,0x03,0x09,0x7D,0x83,0xFF,0x6C,0x9B,0xD0,0xCB,0xCB,0xCB,0x03,0xFB,0xEF
-,0xA0,0xBA,0x9E,0x98,0x27,0x87,0xD4,0x51,0xF1,0x0F,0xED,0x6E,0xF8,0xFA,0xD3,0xCB
-,0xF8,0x7F,0xBC,0xBF,0x27,0x6C,0xE9,0x46,0x09,0xF4,0x9F,0xEC,0x8F,0xE0,0xBB,0x03
-,0x3B,0x14,0xFF,0x8F,0x1F,0xC4,0x3B,0x32,0x25,0x12,0x67,0xE6,0xD8,0x74,0x5E,0x42
-,0xBE,0xCA,0xF4,0x36,0x18,0xDF,0x94,0xC2,0xC1,0x07,0x61,0x87,0xE2,0x19,0x09,0x8E
-,0xDC,0x9C,0xC6,0xE9,0x30,0x4C,0x7B,0xCD,0xF4,0xC9,0xA7,0x02,0xD1,0x4A,0xEE,0x88
-,0x44,0x01,0x70,0xC2,0x43,0x9E,0x0C,0x79,0xDC,0x33,0xEB,0x1B,0x1F,0xAE,0x7B,0xD5
-,0x3B,0xAD,0x1C,0x13,0x0A,0x86,0x22,0x3A,0x96,0xE7,0x12,0xD3,0x1C,0xCB,0xCE,0xE7
-,0xEB,0xF9,0xC6,0xB0,0xBA,0xEE,0x9B,0x65,0xD7,0xFC,0xC6,0x78,0x83,0x13,0x15,0x33
-,0x85,0x50,0xE7,0x12,0x96,0xEE,0x8A,0xB4,0xBE,0x90,0x4E,0x64,0x5A,0x97,0x82,0x6C
-,0x40,0xC7,0xEB,0x40,0x4B,0x79,0xDE,0xE2,0xD1,0xA1,0x60,0xBB,0xFB,0xE9,0xB0,0x7C
-,0xFD,0x4A,0x7A,0xCB,0x1C,0x9E,0xF8,0x0E,0x0B,0xA9,0x9C,0xC8,0x43,0x81,0x1C,0x89
-,0x6F,0xDA,0xF6,0x3F,0x16,0x8F,0x5B,0x43,0x34,0x36,0x15,0xBB,0xEE,0x42,0xE9,0x96
-,0xF6,0xB3,0x69,0xD5,0xF6,0x75,0x3A,0x2D,0xDF,0x6F,0xB7,0x2A,0x05,0x9B,0x8A,0xE5
-,0x7D,0x90,0xFB,0xD9,0xE4,0xC0,0x7F,0x7B,0xF3,0x8A,0x92,0x5A,0xB2,0x1C,0x4A,0x2D
-,0x39,0xE0,0x01,0xFF,0xAE,0xB0,0x29,0xBA,0xEE,0x28,0xC9,0xC3,0xCD,0x1D,0x66,0x9B
-,0x81,0x36,0x21,0x25,0xA8,0xC2,0x95,0x09,0xBD,0xF8,0x85,0xCE,0x48,0xFB,0xD1,0x6A
-,0x05,0x3B,0xAE,0x37,0x98,0xB3,0xAE,0xC4,0xFB,0x7A,0xCD,0x64,0x03,0x09,0xFE,0x0A
-,0xCE,0x1C,0x3C,0xB0,0xFC,0xBD,0x28,0x84,0x3B,0xF3,0x6B,0xF3,0xBD,0x0E,0xB6,0x30
-,0x87,0x0C,0xD6,0xCC,0x10,0x48,0xFC,0xDA,0x35,0x41,0x47,0x9E,0x44,0x05,0xBA,0x71
-,0x67,0xCB,0x46,0xB5,0x61,0x3B,0x9B,0xFF,0x03,0xC7,0x80,0x67,0x9D,0x6C,0x7E,0xFD
-,0xEF,0x6B,0xF2,0x76,0x7E,0x4D,0x8F,0xFC,0xCF,0x1A,0xE7,0xB4,0x0A,0x11,0x60,0x9C
-,0x3F,0x78,0x9F,0x03,0x0C,0xC5,0x5B,0xB0,0x3F,0xFC,0xCC,0xA9,0x69,0x24,0x2E,0x2E
-,0x5F,0x8D,0xB1,0x26,0x6B,0x6F,0x71,0xAD,0x43,0xB7,0x4D,0x56,0x89,0x2E,0xBE,0x8D
-,0xE2,0x7A,0xC0,0xFE,0x8C,0x13,0x72,0x57,0x32,0x55,0x80,0x1D,0x19,0xF9,0x41,0xC8
-,0x25,0xBE,0x18,0xB4,0xD9,0x0D,0xA8,0xF7,0x61,0x4E,0xFE,0xF2,0xD3,0xEF,0xE0,0x1E
-,0xA3,0x84,0xE7,0x47,0xE3,0x8F,0xAD,0xC3,0x79,0xEC,0x56,0x83,0xFE,0x92,0xB9,0x1E
-,0x02,0x4B,0x74,0x0C,0x13,0x82,0x87,0x67,0x94,0x4B,0x91,0xAF,0x3C,0x59,0x74,0x6B
-,0xB3,0xD0,0x35,0xA8,0xB3,0xD3,0xE8,0xF4,0xF5,0xE9,0xDF,0x16,0xE4,0xF2,0x74,0x3E
-,0x45,0xEA,0xAD,0xDC,0x62,0x43,0xA0,0x03,0xAC,0xFF,0x86,0x46,0x90,0xED,0xC8,0x63
-,0xA9,0x8B,0x46,0x6D,0x59,0xB5,0xEF,0x96,0x98,0xC3,0xF8,0x64,0xF2,0x6F,0xAA,0x90
-,0xEC,0xFE,0x71,0xE5,0xDF,0x88,0x5F,0x01,0xFA,0x6C,0x65,0xA5,0x2A,0x0A,0x00,0x00
+,0x2A,0x22,0x78,0x6F,0xC9,0x4F,0x29,0x51,0xAC,0x82,0x5E,0x42,0x09,0xCB,0x9D,0xD0
+,0x2A,0xA1,0xA8,0x7F,0x72,0x12,0x73,0xB1,0x6E,0xF7,0x57,0x5F,0x9D,0xCB,0x8C,0x5E
+,0x81,0x69,0xE5,0x88,0x24,0x40,0x72,0x0B,0xAE,0x9D,0x21,0x45,0x28,0x40,0xF1,0xF4
+,0xE1,0xAF,0xA7,0x27,0x72,0xDB,0xEE,0xBB,0x8A,0xA3,0x4E,0x18,0x76,0x38,0x96,0x49
+,0x08,0x63,0x9C,0x98,0x6E,0x84,0x43,0x9E,0xC6,0x92,0x65,0x20,0x09,0xB2,0x09,0x86
+,0xAC,0x59,0xA3,0x99,0x1B,0xCE,0x0D,0x58,0xEB,0x71,0xFC,0x32,0x46,0xC4,0xF1,0x89
+,0x96,0x50,0x75,0xE3,0x46,0x61,0xA0,0x03,0xD3,0x16,0xA0,0xF3,0x6B,0x0F,0x38,0x02
+,0xC0,0xA1,0x79,0x19,0x97,0x5A,0x1B,0x47,0xD3,0xCF,0xF8,0xFB,0x32,0x22,0xAA,0xA9
+,0x32,0x0C,0xD2,0x40,0xA5,0xD5,0x1F,0x51,0x09,0x78,0x3F,0x45,0xA5,0xB1,0xE8,0xC4
+,0x17,0x1B,0x42,0xFB,0xDA,0x90,0x78,0xE5,0x11,0x8D,0x16,0xEB,0xE7,0x02,0xB2,0xE1
+,0x18,0x0F,0x66,0xED,0x46,0x1B,0xFE,0x32,0x26,0x75,0xB7,0x7B,0x14,0x15,0x04,0x19
+,0x07,0xC5,0x63,0x7E,0x97,0x0D,0x8E,0x86,0xBC,0x89,0xA3,0x71,0xD2,0xC5,0x11,0xE6
+,0x66,0x3B,0x98,0xA4,0x62,0x9F,0x89,0x1F,0x61,0x2D,0x72,0xB0,0xC4,0x69,0xE2,0x0C
+,0xCB,0x57,0x93,0x6C,0x1C,0xFB,0x64,0x73,0xA6,0x6A,0x30,0x42,0x23,0xA3,0xB6,0x78
+,0x12,0xBA,0x11,0xDC,0x95,0x57,0xAC,0x41,0xDD,0x12,0x44,0x51,0xBA,0x76,0x4C,0xD3
+,0x47,0xDC,0x4A,0xC2,0xDE,0xC1,0xF1,0x16,0xEE,0x47,0x09,0x30,0xC6,0x0E,0xDE,0x8E
+,0x25,0xD8,0x0D,0x12,0xFA,0x0E,0xFF,0xD9,0x36,0xA1,0x97,0x97,0x97,0x07,0xF6,0x3F
+,0x40,0x75,0x3D,0x31,0x4F,0x0E,0xA9,0xA3,0xE2,0x27,0xED,0x6E,0xF8,0xFA,0xF3,0xCB
+,0xF8,0xFF,0x79,0x7F,0x4F,0xD8,0xD2,0x8D,0x12,0xE8,0x7F,0xD9,0x1F,0xC1,0x77,0x07
+,0x76,0x28,0x7E,0xAD,0x1F,0x71,0x66,0x8E,0x0D,0xE7,0x25,0xE4,0xAB,0x4C,0x6F,0x83
+,0xE9,0x4D,0x29,0x1C,0xFC,0x21,0xEC,0x50,0x3A,0x23,0xC1,0x91,0x93,0x53,0xF4,0xC3
+,0x20,0xED,0x35,0xD3,0x27,0x9F,0x08,0x44,0x2B,0xB9,0x23,0x12,0x05,0xC0,0x09,0x0F
+,0x59,0x32,0x64,0x71,0xCF,0xAC,0x6F,0x7B,0xB8,0xEE,0x55,0xEF,0xB4,0x72,0x4C,0x28
+,0x18,0x4A,0xE8,0x58,0x9E,0x4B,0x4C,0x72,0x2C,0x3A,0x9F,0xAD,0xE7,0x1B,0xC3,0xEA
+,0xBA,0x6F,0x95,0x5D,0xEB,0x1B,0xE3,0x0D,0x4E,0x54,0xCC,0x14,0x42,0x9D,0x4B,0x58
+,0xBA,0x2B,0xD2,0xFA,0x42,0x3A,0x91,0x69,0x5D,0x0A,0xB2,0x01,0x1D,0xAF,0x04,0x2D
+,0xE5,0x79,0x8B,0x47,0x87,0x72,0xED,0xEE,0xA8,0xC3,0xE2,0xF5,0x2B,0xE9,0x2D,0x73
+,0x78,0xDE,0x3B,0x2C,0xA3,0x72,0x22,0x0F,0xE5,0x71,0x24,0xBE,0x69,0x9B,0xFF,0x58
+,0x3C,0x6E,0x0C,0xD1,0xD8,0x54,0xEC,0xBA,0x4B,0xA5,0x5B,0xDA,0xCF,0xA6,0x35,0xDB
+,0x57,0xE9,0xB4,0x78,0xBF,0xDF,0xA8,0x14,0x6C,0x2A,0x96,0xF7,0x41,0xEE,0x67,0x93
+,0x03,0xFF,0xED,0xDD,0x1B,0x4A,0x6A,0xC9,0x72,0x28,0xB5,0xE4,0x80,0x07,0xFC,0xBB
+,0xC2,0x96,0xE8,0xBA,0xA3,0x24,0x0F,0x37,0x77,0x98,0xF4,0x06,0xDA,0x74,0x94,0xA0
+,0x0A,0x57,0x26,0xF4,0xE2,0x17,0x3A,0x23,0xED,0x47,0xAB,0x15,0xEC,0xB8,0xDE,0x60
+,0xC6,0xBA,0x12,0xEF,0xEC,0x35,0x93,0x0D,0x24,0xF8,0x2B,0x38,0x73,0xF0,0xC0,0xF2
+,0x8F,0xA2,0x10,0xEE,0xCC,0xAF,0xCD,0xF7,0x3A,0xD8,0xC0,0x1C,0x32,0x58,0x33,0x43
+,0x20,0xF1,0x6B,0xD7,0x04,0x1D,0x79,0x12,0x15,0xE8,0xC6,0x9D,0x2D,0x1B,0xD5,0x86
+,0xED,0x6C,0xFE,0x2F,0x1C,0x03,0x9E,0x75,0xB2,0xF9,0xF5,0x7F,0x6F,0xC9,0xFB,0xF9
+,0x35,0x3D,0xF2,0x3F,0x6B,0x9C,0xD3,0x2A,0x44,0x80,0x71,0xFE,0xE0,0x7D,0x0E,0x30
+,0x14,0xEF,0xC0,0xFE,0xF0,0x33,0xA7,0xA6,0x91,0xB8,0xB8,0x7C,0x33,0xC6,0x9A,0xAC
+,0xBD,0xC7,0xB5,0x0E,0xDD,0x36,0x59,0x25,0xBA,0xF8,0x36,0x8A,0xEB,0x01,0xFB,0x0B
+,0x4E,0xC8,0x5D,0xC9,0x54,0x01,0x76,0x64,0xE4,0x95,0x90,0x4B,0x7C,0x2F,0x68,0xB3
+,0x1B,0x50,0xEF,0xC3,0x9C,0xFC,0xED,0xA7,0x3F,0xC0,0x3D,0x46,0x09,0x8F,0x8F,0xC6
+,0x1F,0x5B,0x87,0xF3,0xD8,0xAD,0x06,0xFD,0x25,0x73,0x3D,0x04,0x96,0xE8,0x18,0x26
+,0x04,0x0F,0xCF,0x28,0x97,0x22,0x5F,0x79,0xB2,0xE8,0xD6,0x66,0xA1,0x6B,0x50,0x67
+,0xA7,0xD1,0xE9,0xDB,0xD3,0x7F,0x2C,0xC8,0xE5,0xE9,0x7C,0x8A,0xD4,0x5B,0xB9,0xC5
+,0x86,0x40,0x07,0x58,0xFF,0x0D,0x8D,0x20,0xDB,0x91,0xC7,0x52,0x17,0x8D,0xDA,0xB2
+,0x6A,0xDF,0x2B,0x31,0x87,0xF1,0xC1,0xE4,0xDF,0x55,0x21,0xD9,0xFD,0x03,0xCB,0xBF
+,0x13,0xBF,0x01,0xED,0xD5,0xFD,0xA0,0x2E,0x0A,0x00,0x00
 };

main/html/config.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
   if (factory == "true") {
     url += "&factory=true";
   }
-
+  
   $.get(url, function (data) {
     console.log(data);
     if (!data.mqtt_enabled)
@@ -41,10 +41,33 @@ function getUrlParameter(sParam) {
 
 $(function () {
   $('#configure').click(function (e) {
-    var d = $('#form :input[name!="newmac"]').serialize({
-      checkboxesAsBools: true
+
+    const form = document.querySelector('#configform');
+    const data = new FormData(form);
+    var checkbox = $("#configform").find("input[type=checkbox]");
+    $.each(checkbox, function (key, val) {
+      data.append($(val).attr('name'), $(val).is(':checked'))
     });
-    console.log(d);
+
+    var d = Array.from(data).reduce((acc, e) => {
+      if(e[0] == "newmac")
+        return acc;
+      if( e[0].includes(":"))
+      {
+        [g, k, v] = [...e[0].split(':'), e[1]];
+        if (!acc[g])
+          acc[g] = {};
+        acc[g][k]=v;
+      }
+      else 
+      {
+        k = e[0]; v= e[1];
+        acc[k] = v;
+      }
+      return acc;
+    }, {}
+    );
+
     $.ajax({
       type: "POST",
       url: "/updateconfig",
@@ -77,15 +100,15 @@ $(function () {
   });
 });
 
-function addMac(mac, whitelist) {
+function addMac(mac, devProp) {
   container = $('#devList > tbody:last-child');
   raw = '<tr id="rw_' + mac + '">';
-  raw += '<td><input type="checkbox" id="' + mac + '"name="' + mac + '"style="width:auto;height:auto"></td>';
-  raw += '<td>' + mac + '</td>';
+  raw += '<td><input type="checkbox" id="'+ mac + '_batt" name="' + mac + ':batt" style="width:auto;height:auto"></td>';
+  raw += '<td>' + mac + '<input type="text" placeholder="Insert a friendly name here" id="' + mac + '_desc" name="' + mac + ':desc" style="font-size:0.95em;text-align:center;width:15em;height:auto" value="' + (devProp.desc ? devProp.desc : "")  +'"><br><hr></td>';
   raw += '<td><input type="button" id="rm_' + mac + '" value="Remove"style="width:auto;height:auto" class=dangerbtn onclick="$(\'#rw_' + mac + '\').remove()"></td>';
   raw += '</tr>';
   container.append(raw);
-  $('#' + mac).prop("checked", whitelist);
+  $('#' + mac +'_batt').prop("checked", devProp.battery);
 }
 
 function validateMacDigit(elem) {
@@ -121,44 +144,3 @@ function isMacValid(mac) {
   return (/^[0-9A-F]+$/.test(mac));
 }
 
-(function ($) {
-  $.fn.serialize = function (options) {
-    return $.param(this.serializeArray(options));
-  };
-
-  $.fn.serializeArray = function (options) {
-    var o = $.extend({
-      checkboxesAsBools: false
-    }, options || {});
-
-    var rselectTextarea = /select|textarea/i;
-    var rinput = /text|hidden|password|search|number/i;
-
-    return this.map(function () {
-      return this.elements ? $.makeArray(this.elements) : this;
-    })
-      .filter(function () {
-        return this.name && !this.disabled &&
-          (this.checked ||
-            (o.checkboxesAsBools && this.type === 'checkbox') ||
-            rselectTextarea.test(this.nodeName) ||
-            rinput.test(this.type));
-      })
-      .map(function (i, elem) {
-        var val = $(this).val();
-        return val == null ?
-          null :
-          $.isArray(val) ?
-            $.map(val, function (val, i) {
-              return {
-                name: elem.name,
-                value: val
-              };
-            }) : {
-              name: elem.name,
-              value: (o.checkboxesAsBools && this.type === 'checkbox') ?
-                (this.checked ? true : false) : val
-            };
-      }).get();
-  };
-})(jQuery);

commit db60cd4c8271e94f8854dce8b98d9cedce00c69f

Fix bug introduced with PR #41

main/settings.cpp
@@ -186,7 +186,7 @@ String Settings::toJSON()
 bool Settings::IsTraceable(const String &value)
 {
     if (enableWhiteList)
-        return IsPropertyForDeviceEnabled(value, DeviceProperty::eReadBattery);
+        return IsPropertyForDeviceEnabled(value, DeviceProperty::eTraceable);
     else
         return true;
 }
@@ -194,7 +194,7 @@ bool Settings::IsTraceable(const String &value)
 bool Settings::InBatteryList(const String &value)
 {
 #if PUBLISH_BATTERY_LEVEL
-    return IsPropertyForDeviceEnabled(value, DeviceProperty::eTraceable);
+    return IsPropertyForDeviceEnabled(value, DeviceProperty::eReadBattery);
 #else
     return false;
 #endif